c2518f4e72f2e68d44d59d8f819568d91876917e
MuniBalaji07 <balajiputthuru07@gmail.com>
2024-08-26 17:05:29 +0530

    creating expense-tracker project

---
+++ expense/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  
+  return(
+    <div className="App">
+    <div>
+      <h1>Expense Tracker</h1>
+      </div>
+   </div> 
+  )
+}
+
+export default App;
