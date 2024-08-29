25fb585963a06af9ea1b2627bd06759459c32a59
MuniBalaji07 <balajiputthuru07@gmail.com>
2024-08-29 16:48:03 +0530

    add income to the project

--- expense/src/App.jsx
+++ expense/src/App.jsx
@@ -2,11 +2,38 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[incomes, setIncomes] = useState([]);
+  
+
+  const addIncome = (e) => {
+    e.preventDefault();
+    if (incomeAmount) {
+      setIncomes([...incomes, { id: Math.random(), amount: parseFloat(incomeAmount), method }]);
+      setIncomeAmount('');
+      setMethod('Cash'); 
+    }
+  };
   
   return(
     <div className="App">
-    <div>
-      <h1>Expense Tracker</h1>
+       <h1>Expense Tracker</h1>
+      <div className="income-input">
+        <h3>Add Income</h3>
+        <form onSubmit={addIncome}>
+        <div>
+            <label htmlFor="income">Amount</label>
+            <input
+              type="number"
+              value={incomeAmount}
+              onChange={(e) => setIncomeAmount(e.target.value)}
+              placeholder="Enter amount..."
+            />
+          </div>
+        
+
+        </form>
+        
+      
       </div>
    </div> 
   )
